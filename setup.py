7fb4299174fa8bb3c61bc89c7c9389390a9dab37
konstin <konstin@mailbox.org>
2020-12-23 21:28:14 +0100

    Publish fork on pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CPCProt",
+    name="bio_embeddings_CPCProt",
     version="0.0.1",
     author="Amy Lu",
     author_email="amyxlu@cs.toronto.edu",
@@ -13,15 +13,14 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Linux",
+        "Operating System :: POSIX :: Linux",
     ],
     python_requires=">=3.6",
     install_requires=[
         "numpy>=1.19,<2.0",
         "torch>=1.0,<2.0",
         "lmdb>=1.0,<2.0",
-        # "tape-proteins>=0.4,<=1.0",
-        "tape-proteins @ git+https://github.com/konstin/tape@patch-1",
+        "bio-embeddings-tape-proteins>=0.4,<=1.0",
         "scipy>=1.5,<2.0",
     ],
 )
